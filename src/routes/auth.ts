a0976b2cc6c0b261c997a64704e4028786f269b5
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-17 00:35:02 +0530

    updated

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,6 +6,13 @@ import bcrypt from "bcrypt"
 import cookie from "cookie"
 import auth from "../middleware/auth"
 
+const mappedErrors = (errors: Object[]) => {
+    return errors.reduce((prev:any, err:any) => {
+        prev[err.property] = Object.entries(err.constraints)[0][1]
+        return prev
+    },{})
+}
+
 const register = async (req: Request, res: Response) => {
     const {email, username, password} = req.body;
 
@@ -19,6 +26,7 @@ const register = async (req: Request, res: Response) => {
         if(usernameUser) errors.username = "Username is already taken"
 
         if(Object.keys(errors).length > 0){
+            
             return res.status(400).json(errors)
         }
 
@@ -26,7 +34,7 @@ const register = async (req: Request, res: Response) => {
         const user = new User({ email, username, password });
         errors = await validate(user)
         if(errors.length > 0){
-            return res.status(400).json({errors})
+            return res.status(400).json(mappedErrors(errors));
         }
         await user.save();
 
