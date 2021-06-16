571303391ccc1d86057d1cec89fcf898bb4f1b4c
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-16 19:02:28 +0530

    added entities

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import express from "express";
+import morgan  from "morgan";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config()
+
+import authRoutes from './routes/auth'
+import postRoutes from './routes/post'
+import subRoutes from './routes/sub'
+import trim from './middleware/trim'
+
+const app = express();
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(morgan('dev'));
+app.use(trim);
+app.use(cookieParser());
+
+app.get('/',(_,res) => res.send('Hello World!'));
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/subs', subRoutes);
+
+app.listen(port, async () => {
+    console.log(`Server is running at http://localhost:${port}`)
+
+    try {
+        await createConnection()
+        console.log("DATABASE CONNECTED");
+    } catch (error) {
+        console.log("error", error)
+    }
+})
