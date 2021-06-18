bee8244e5069827459de51d6577dfea676f82ac8
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-18 12:59:54 +0530

    added meta tags

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,23 +1,43 @@
 import { AppProps } from "next/app";
 import Axios from "axios"
 import '../styles/tailwind.css'
+import '../styles/icons.css'
 import {useRouter} from 'next/router'
-
+import {AuthProvider} from '../context/auth'
+import { SWRConfig } from 'swr'
 import Navbar from '../components/Navbar'
-import { Fragment } from "react";
 
-Axios.defaults.baseURL = 'http://localhost:5000/api'
+
+Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL+ '/api'
 Axios.defaults.withCredentials = true
+
+const fetcher = async (url: string) => {
+  try {
+    const res = await Axios.get(url)
+    return res.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
 
 
 function App({ Component, pageProps }: AppProps) {
   const {pathname} = useRouter();
   const authRoutes = ['/register', '/login']
   const authRoute = authRoutes.includes(pathname)
-  return <Fragment>
+  return <SWRConfig       
+  value={{
+      fetcher,
+      dedupingInterval: 10000,
+    }}
+  >
+  <AuthProvider>
     {!authRoute && <Navbar/>}
+    <div className={authRoute ? '' : 'pt-14 '}>
     <Component {...pageProps} />
-  </Fragment>
+    </div>
+  </AuthProvider>
+  </SWRConfig>
 }
 
 export default App;
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -16,7 +16,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>The Pit Stop</title>
+          <link rel="icon" type="image/png" href="/pitstop.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <meta property="og:site_name" content="The Pit Stop" />
+          <meta property="twitter:site" content="@jaydjhangiani" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/pitstop.png`} />
+          <meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/pitstop.png`} />
+          <meta property="twitter:card" content="We at ‘The Pit Stop’ believe in equipping you with all that you need to know about current affairs, entertainment, technology, sport, fashion.... your window to the world!" />
+
           <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600&display=swap"
             rel="stylesheet"
