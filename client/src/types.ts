d5cf4aa6f8f2ae7c933804355f3b405e1a474672
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-18 12:59:37 +0530

    added types

--- client/src/types.ts
+++ client/src/types.ts
@@ -7,6 +7,45 @@ export interface Post{
     subName: string;
     createdAt: string;
     updatedAt: string;
+    sub?: Sub
     //virtual fields
     url: string;
+    voteScore?: number
+    commentCount?: number
+    userVote?: number
 }
+
+export interface User {
+    username: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Sub {
+    createdAt: string
+    updatedAt: string
+    name: string
+    title: string
+    description: string
+    imageUrn: string
+    bannerUrn: string
+    username: string
+    posts: Post[]
+    // Virtuals
+    imageUrl: string
+    bannerUrl: string
+    postCount?: number
+  }
+
+  export interface Comment {
+    identifier: string
+    body: string
+    username: string
+    createdAt: string
+    updatedAt: string
+    post?: Post
+    // Virtuals
+    userVote: number
+    voteScore: number
+  }
